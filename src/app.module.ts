c92bac8cf2df8d741a7aebd97c5461190acbd8cc
taesik <taesikyoon97@naver.com>
2023-10-25 21:02:53 +0900

    초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PostModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
